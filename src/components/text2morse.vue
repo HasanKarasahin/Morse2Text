016965f629109e4512da5cfa26270af7576cb0da
Hasan Gazi <hasn_07@hotmail.com>
2019-04-05 20:01:46 +0300

    text2morse templates eklendi.

--- src/components/text2morse.vue
+++ src/components/text2morse.vue
@@ -1,4 +1,33 @@
 ﻿<template>
+    <div style="margin:5px">
+        <b-form @reset="onReset">
+
+            <b-form-group id="input-group-1"
+                          :label=label1
+                          label-for="input-1">
+                <b-form-textarea id="textarea-no-auto-shrink"
+                                 rows="3"
+                                 v-model:value="message1"
+                                 max-rows="8"
+                                 no-auto-shrink></b-form-textarea>
+            </b-form-group>
+
+            <b-button style="margin-bottom:5px" variant="primary" v-on:click="onSubmit">Convert</b-button>
+
+            <b-form-group id="input-group-1"
+                          :label=label2
+                          label-for="input-1">
+                <b-form-textarea id="textarea-no-auto-shrink"
+                                 rows="3"
+                                 v-model:value="message2"
+                                 max-rows="8"
+                                 no-auto-shrink></b-form-textarea>
+            </b-form-group>
+
+
+            <b-button type="reset" variant="danger">Reset</b-button>
+        </b-form>
+    </div>
 </template>
 
 <script>
