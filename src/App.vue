22ec31e13e9b9dbfe104f10296a5c7015ddae0d4
Hasan Gazi <hasn_07@hotmail.com>
2019-04-05 20:07:15 +0300

    app template eklendi.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
     <div id="app">
-        <Home msg="Hello world!" />
+
+        <b-alert variant="success" show>
+            <b-button type="submit" style="margin-left:5px" :variant="[state?'primary':'danger']" v-on:click="morse2textclick">Morse To Text</b-button>
+            <b-button type="submit" style="margin-left:5px" :variant="[!state?'primary':'danger']" v-on:click="text2morseclick">Text To Mors</b-button>
+        </b-alert>
+
+        <div v-if="state">
+            <text2morse title="text2morse" label1="Text" label2="Morse" />
+        </div>
+
+        <div v-else>
+            <morse2text title="morse2text" label1="Morse" label2="Text" />
+        </div>
+
     </div>
 </template>
 
 <script>
-    import Home from './components/Home.vue';
+
+    <script>
+        import Home from './components/Home.vue';
 
     export default {
-        name: 'app',
+            name: 'app',
         components: {
             Home
         }
-    };
+        };
 </script>
 
 <style>
