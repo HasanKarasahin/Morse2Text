e067d123835368ea1e770ff19132b915a0255548
Hasan Gazi <hasn_07@hotmail.com>
2019-04-05 20:00:49 +0300

    morse2text script eklendi.

--- src/components/morse2text.vue
+++ src/components/morse2text.vue
@@ -31,6 +31,83 @@
 </template>
 
 <script>
+
+    var alphabet_morse = {
+        "-----": "0",
+        ".----": "1",
+        "..---": "2",
+        "...--": "3",
+        "....-": "4",
+        ".....": "5",
+        "-....": "6",
+        "--...": "7",
+        "---..": "8",
+        "----.": "9",
+        ".-": "a",
+        "-...": "b",
+        "-.-.": "c",
+        "-..": "d",
+        ".": "e",
+        "..-.": "f",
+        "--.": "g",
+        "....": "h",
+        "..": "i",
+        ".---": "j",
+        "-.-": "k",
+        ".-..": "l",
+        "--": "m",
+        "-.": "n",
+        "---": "o",
+        ".--.": "p",
+        "--.-": "q",
+        ".-.": "r",
+        "...": "s",
+        "-": "t",
+        "..-": "u",
+        "...-": "v",
+        ".--": "w",
+        "-..-": "x",
+        "-.--": "y",
+        "--..": "z",
+        "/": " ",
+        "-·-·--": "!",
+        "·-·-·-": ".",
+        "--··--": ","
+    };
+
+    function morseToText(morse_text) {
+        var messageConverted = [];
+
+        morse_text.split("/").map(function (word) {
+            word.split(" ").map(function (letter) {
+                messageConverted.push(alphabet_morse[letter]);
+            });
+            messageConverted.push(" ");
+        });
+
+        return messageConverted.join("");
+    }
+
+    export default {
+        name: 'text2morse',
+        props: {
+            title: String,
+            label1: String,
+            label2: String,
+            message1: '',
+            message2: ''
+        },
+        methods: {
+            onSubmit() {
+                var temp = morseToText(this.message1);
+                this.message2 = temp;
+            },
+            onReset(evt) {
+                this.message1 = ''
+                this.message2 = ''
+            }
+        }
+    };
 </script>
 
 <style scoped>
