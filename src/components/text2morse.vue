65f04f67b1935312d5813e1034e3612d7f95a70c
Hasan Gazi <hasn_07@hotmail.com>
2019-04-05 20:04:17 +0300

    text2morse script eklendi.

--- src/components/text2morse.vue
+++ src/components/text2morse.vue
@@ -31,6 +31,89 @@
 </template>
 
 <script>
+
+    var alphabet_latin = {
+        "0": "-----",
+        "1": ".----",
+        "2": "..---",
+        "3": "...--",
+        "4": "....-",
+        "5": ".....",
+        "6": "-....",
+        "7": "--...",
+        "8": "---..",
+        "9": "----.",
+        "a": ".-",
+        "b": "-...",
+        "c": "-.-.",
+        "d": "-..",
+        "e": ".",
+        "f": "..-.",
+        "g": "--.",
+        "h": "....",
+        "i": "..",
+        "j": ".---",
+        "k": "-.-",
+        "l": ".-..",
+        "m": "--",
+        "n": "-.",
+        "o": "---",
+        "p": ".--.",
+        "q": "--.-",
+        "r": ".-.",
+        "s": "...",
+        "t": "-",
+        "u": "..-",
+        "v": "...-",
+        "w": ".--",
+        "x": "-..-",
+        "y": "-.--",
+        "z": "--..",
+        " ": "/",
+        "!": "-·-·--",
+        ".": "·-·-·-",
+        ",": "--··--"
+    };
+
+    function textToMorse(latin_text) {
+        var messageConverted = [];
+        latin_text = latin_text.split(" ");
+        var sayac = 0;
+        latin_text.map(function (word) {
+            sayac++;
+            var i;
+            for (i = 0; i < word.length; i++) {
+                messageConverted.push(alphabet_latin[word[i]]);
+            }
+
+            if (sayac < latin_text.length)
+                messageConverted.push("/");
+        });
+
+        return messageConverted.join(" ");
+    }
+
+    export default {
+        name: 'text2morse',
+        props: {
+            title: String,
+            label1: String,
+            label2: String,
+            message1: '',
+            message2: ''
+        },
+        methods: {
+            onSubmit() {
+                var temp = this.message1;
+                temp = textToMorse(temp.toLowerCase());
+                this.message2 = temp;
+            },
+            onReset(evt) {
+                this.message1 = ''
+                this.message2 = ''
+            }
+        }
+    }
 </script>
 
 <style scoped>
